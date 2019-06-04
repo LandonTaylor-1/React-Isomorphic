08e37a02169b93fb177164e9417de004b92e9ae3
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-06-04 11:19:02 -0500

    isomorphic

---
+++ src/Beer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Beer.css'
+
+const Beer = (props) => {
+    //console.log('props', props)
+    return (
+        <div className='body'>
+            <div className='beers'>
+                <img alt="Beer" src={props.beer.image_url}></img><br></br>
+                Name: {props.beer.name}<br></br>
+                ABV:{props.beer.abv}% - IBU:{props.beer.ibu}<br></br>
+                <button onClick={()=>props.like(props.id)}>LIKE</button>
+            </div>
+        </div>
+    )
+}
+
+export default Beer
